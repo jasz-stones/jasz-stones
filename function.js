9fda2fcb5bad42a4a54fe8ee7847a38a65f06553
jasz-stones <jazstones0@gmail.com>
2025-10-26 23:59:10 +0100

    Add files via upload

---
+++ function.js
@@ -0,0 +1,39 @@
+function Fooldal(){
+    window.location.href = "index.html"
+}
+
+function Érdekel(){
+    window.location.href = "0.html"
+}
+
+function Érdekel1(){
+    window.location.href = "1.html"
+}
+
+function Érdekel2(){
+    window.location.href = "2.html"
+}
+
+function Érdekel3(){
+    window.location.href = "3.html"
+}
+
+function Érdekel4(){
+    window.location.href = "4.html"
+}
+
+function Érdekel5(){
+    window.location.href = "5.html"
+}
+
+function Érdekel6(){
+    window.location.href = "6.html"
+}
+
+function Érdekel7(){
+    window.location.href = "7.html"
+}
+
+function Érdekel8(){
+    window.location.href = "8.html"
+}
